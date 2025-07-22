4d3992271f1820bfa29379705f54e1ab0f8eef4a
Jose María <joselaspalas@gmail.com>
2025-07-22 12:35:50 +0200

    ejercicios hasta el 14 hechos

---
+++ ejercicio_14.js
@@ -0,0 +1,22 @@
+const words = [
+  'code',
+  'repeat',
+  'eat',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code',
+  'enjoy',
+  'sleep',
+  'code'
+];
+function repeatCounter(list) {
+    for(let i = 0; i < list.length; i++){
+        let counter = 0;
+        if (list.includes[i])
+    }
+
+}
+
+console.log(repeatCounter(words));
